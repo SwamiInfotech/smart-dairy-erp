39baef03eb372cfe0bd078fbc0f70f135b73d83a
SwamiInfotech <142709968+SwamiInfotech@users.noreply.github.com>
2026-07-03 20:57:30 +0530

    initialise project

---
+++ backend/smart-dairy-backend/src/main/java/com/smartdairy/SmartDairyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartdairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDairyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDairyBackendApplication.class, args);
+	}
+
+}
